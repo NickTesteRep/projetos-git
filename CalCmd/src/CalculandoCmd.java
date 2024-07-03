6f7abd897626c34d3bae3327acdd2d346ab803b3
NickDevD <nick.developerdev@gmail.com>
2024-07-03 07:42:20 -0300

    Funções adicionadas

--- CalCmd/src/CalculandoCmd.java
+++ CalCmd/src/CalculandoCmd.java
@@ -21,7 +21,7 @@ public class CalculandoCmd {
     int multiplicacao;
     float divisao;
 
-
+    
     //Cmd interativo
    show.println("Digite seu nome: ");
         String texto = scan.next();
@@ -30,35 +30,51 @@ public class CalculandoCmd {
    show.println("2-Subtracao");
    show.println("3-multiplicacao");
    show.println("4-Divisao");
-        int texto2 = scan.nextInt();     
+         
     
-
-        switch (texto2) {
+        while (true) {
+            
+            int texto2 = scan.nextInt();    
+        
+            switch (texto2) {
             case 1:
-            show.println("Soma escolhida!");
-            show.print("Digite o primeiro numero: ");
-            int som1 = scan.nextInt();
-            show.println("Digite o segundo numero: ");
-            int som2 = scan.nextInt();
-            soma = som1 + som2;
-            show.println("O resultado da operacao é: " + soma);
+                show.println("Soma escolhida!");
+                show.print("Digite o primeiro numero: ");
+                    int som1 = scan.nextInt();
+                show.println("Digite o segundo numero: ");
+                    int som2 = scan.nextInt();
+                soma = som1 + som2;
+                show.println("O resultado da operacao é: " + soma);
                 break;
         
             case 2:
-            show.println("Subtracao escolhida!");
-            show.println("Digite o primeiro numero: ");
-            int sub1 = scan.nextInt();
-            show.println("Digite o segundo numero: ");
-            int sub2 = scan.nextInt();
-            subtracao = sub1 - sub2;
-            show.println("O resultado da operacao é: " + subtracao);
+                show.println("Subtracao escolhida!");
+                show.println("Digite o primeiro numero: ");
+                    int sub1 = scan.nextInt();
+                show.println("Digite o segundo numero: ");
+                    int sub2 = scan.nextInt();
+                subtracao = sub1 - sub2;
+                show.println("O resultado da operacao é: " + subtracao);
                 break;
 
             case 3:
-            show.println("Multiplicacao escolhida!");
-            show.println("Digite o primeiro numero: ");
-            int mult1 = scan.nextInt();
+                show.println("Multiplicacao escolhida!");
+                show.println("Digite o primeiro numero: ");
+                    int mult1 = scan.nextInt();
+                show.println("Digite o segundo numero");
+                    int mult2 = scan.nextInt();
+                multiplicacao = mult1 * mult2;
+                show.println("O resultado da operação é: " + multiplicacao);
+                break;
+            
+            case 0: 
+                System.err.println("Programa encerrado.");
+                return;
+            default:
+                System.err.println("Operação invalida. Tente novamente");
+            }
         }
-    }  
+    }
+}  
     
-}
+
