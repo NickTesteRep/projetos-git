4929aa6a733da3178324eca9310a883ac4dc301e
NickDevD <nick.developerdev@gmail.com>
2024-07-04 15:59:53 -0300

    Readme adicionado

--- SistemaBancario/src/SisSaquesDepositos.java
+++ SistemaBancario/src/SisSaquesDepositos.java
@@ -25,9 +25,7 @@ public class SisSaquesDepositos {
                 finally{
                     scan.close();
                 }
-                
-                
-                 
+                                        
             
         while (true) {
             
